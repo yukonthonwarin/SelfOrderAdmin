60d0ce8dc16d887b951e3f10e728b79ef89f7bf6
yukonthonwarin <104727871+yukonthonwarin@users.noreply.github.com>
2022-08-29 10:46:12 +0700

    หน้าloginกับหน้าhome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full",redirectTo: "login"},
   {path:"login",component: LoginComponent},
+  {path:"home",component: HomeComponent}
 ];
 
 @NgModule({
