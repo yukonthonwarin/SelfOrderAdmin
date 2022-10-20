a4bcf49b7b5e8631fb4c7299bdee60d4a042d9bf
yukonthonwarin <104727871+yukonthonwarin@users.noreply.github.com>
2022-10-20 16:39:48 +0700

    no message

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiserviceService } from '../apiservice.service';
+import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-checklist',
   templateUrl: './checklist.component.html',
   styleUrls: ['./checklist.component.css']
 })
 export class ChecklistComponent implements OnInit {
+  dataList : any = [];
+  sub : any;
+  
+  a_order_hdr : any = '0';
 
-  constructor() { }
+  constructor(private _Activatedroute:ActivatedRoute,private api: ApiserviceService) { }
 
   ngOnInit(): void {
+    this.sub=this._Activatedroute.paramMap.subscribe(params => { 
+      console.log(params);
+      this.a_order_hdr = params.get('a_order_hdr');  
+    }); 
+    this.doGet();
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+  
+  
+
+  async doGet(){ 
+    let today : any = moment().format('YYYY-MM-DD');
+    let r1 : any  = await this.api.get("order.list?e_action=print_bill&date="+today).toPromise(); 
+    if(r1['success']){
+      this.dataList = [];
+      for (const key in r1.c_data) {
+        let element = r1.c_data[key];
+        element['f_doc_amt'] = 0;
+        element['_check'] = false;
+        if(element['hdr']['a_order_hdr']==this.a_order_hdr){
+          element['_check'] = true;
+        }
+        for (const k2 in element['dtl']) {
+          const d = element['dtl'][k2];
+          element['f_doc_amt']  += (d['f_price_total']*1); 
+        }
+        this.dataList.push(element);
+      }
+    } 
+
+  }
 }
--- src/app/setting/recommend/recommend.component.ts
+++ src/app/setting/recommend/recommend.component.ts
@@ -11,22 +11,24 @@ export class RecommendComponent implements OnInit {
   currentMenuId: any  = '' ;
   itemList : any = [];
   itemObjList : any = {};
-
+  
   menuList : any = [];
   menuObjList : any = {};
 
+  currentRow : any;
+  isEdit = false;
   dataList : any;
   myForm: FormGroup;
-
+  mode:any;
   constructor(private api: ApiserviceService, private formBuilder: FormBuilder) {
     this.myForm = this.formBuilder.group({
       lng1_c_name: ['' , Validators.required],
       lng2_c_name: [''],
-      item_id: ['' , Validators.required ], 
+      c_seq: ['' ], 
       e_status: ['active' ], 
       f_price :  [0 , Validators.required],
-      menu_id: [''],
       
+      menu_id: ['', Validators.required ],  
       
     });
    }
@@ -34,28 +36,31 @@ export class RecommendComponent implements OnInit {
   ngOnInit(): void {
     this.doGet();
     this.doGetItem();
-    this.doGetMenu();
+    
   }
 
   async doGet(){ 
     let _w = "";
     if(this.currentMenuId!=undefined && this.currentMenuId!=""){
-      _w = "?menu_id="+this.currentMenuId;
+      _w = "?item_id="+this.currentMenuId;
     }
     let mDataArray : any  = await this.api.get("fr.setrecommend.list"+_w).toPromise(); 
     this.dataList =  mDataArray.c_data;
   }
+
+  
 
   async doGetItem(){
     let mDataArray : any  = await this.api.get("fr.setitem.list").toPromise(); 
     this.itemList =  mDataArray.c_data;    
     for (const key in this.itemList) {
       const e = this.itemList[key];
-      this.itemObjList[e.item_id] = e; 
+      this.itemObjList[e.item_id] = e;
+       
     }
   }
   async doGetMenu(){
-    let mDataArray : any  = await this.api.get("fr.setitem.list").toPromise(); 
+    let mDataArray : any  = await this.api.get("fr.setmenu.list").toPromise(); 
     this.menuList =  mDataArray.c_data;    
     for (const key in this.menuList) {
       const e = this.menuList[key];
@@ -63,4 +68,51 @@ export class RecommendComponent implements OnInit {
     }
   }
   
+  
+  async doPost(){
+
+    let payload = this.myForm.value;
+    if(this.myForm.valid){ 
+
+      payload.c_media_name = this.currentRow.c_media_name;
+      payload.media_id = this.currentRow.media_id;
+
+      if(this.currentRow.item_id!=undefined)
+        payload.item_id = this.currentRow.item_id;
+
+      console.log(payload); 
+      this.save(payload); 
+    } 
+  }
+
+  async save(payload:any){
+    let result = await this.api.post("fr.setitem.save", payload).toPromise();  
+      console.log(result);
+      if(result.success){
+         this.api.showSwal('success', 'Save Complete', '');
+         this.cancel();
+         this.doGet();
+      }
+  }
+
+  cancel(){
+    this.isEdit = false;
+  }
+
+  add(){
+    this.mode = "เพิ่ม";
+    this.isEdit = true;
+    let row = {
+      lng1_c_name: '',
+      lng2_c_name: '',
+      c_seq: '0',
+      e_status: 'active', 
+      f_price : 0,
+      uofm_id : '0',
+      menu_id : this.menuList[0].menu_id,
+      printer_id : '0',
+    };
+    this.currentRow = row;
+    this.myForm.setValue(row); 
+  }
 }
